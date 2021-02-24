ae1e99628dd0d7ce5597967b0b997b10a13e785c
Matt Reilly <kb1vc@kb1vc.org>
2021-02-24 17:30:50 -0500

    Fixes to Format.hxx -- it needed to include stdexcept
    Moved some CMake stuff around.

---
+++ cmake/FindSoDaUtils.cmake
@@ -0,0 +1,26 @@
+# Find the SoDaUtils library
+message("In FindSoDaUtils.cmake")
+# This defines:
+
+#  SoDaUtils_INCLUDE_DIR contains SoDa/*.h
+#  SoDaUtils_LIBRARIES points to libsodautils...
+#  SoDaUtils_FOUND true if we found it. 
+
+find_path(SoDaUtils_INCLUDE_DIR
+  NAMES SoDa/Format.hxx
+  )
+
+find_library(SoDaUtils_LIBRARY sodautils)
+
+message("include ${SoDaUtils_INCLUDE_DIR}")
+message("lib ${SoDaUtils_LIBRARY}")
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(SoDaUtils
+  REQUIRED_VARS SoDaUtils_LIBRARY SoDaUtils_INCLUDE_DIR)
+
+if(SoDaUtils_FOUND)
+  set( SoDaUtils_LIBRARIES ${SoDaUtils_LIBRARY} )
+endif()
+
+mark_as_advanced(SoDaUtils_INCLUDE_DIR SoDaUtils_LIBRARY)
--- include/Format.hxx
+++ include/Format.hxx
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <exception>
+#include <stdexcept>
 #include <iostream>
 #include <list>
 
